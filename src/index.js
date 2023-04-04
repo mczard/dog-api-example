e8ccbb494f389ad5ac07760d4902384eb2e6b4d7
Mateusz Czardybon <mateusz.czardybon@gmail.com>
2023-04-04 20:46:31 +0200

    githubpages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { Menu } from './components/menu';
-import { Gallery } from './components/gallery';
-import { Filter } from './components/filter';
-import css from './css/index.css';
-import bootstrap from 'bootstrap/dist/css/bootstrap.css'
-
-new Menu("menu");
-new Gallery("gallery");
-new Filter("filter");
